7772ef51006ded4f735e154c54b14b6a15e25a2a
2faf151 <automation.argon@gmial.com>
2025-06-18 13:24:14 +0200

    v37

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Script from "next/script";
 
 import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
@@ -24,6 +25,18 @@ export default function DefaultLayout({
 
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-PV01NN456D"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-PV01NN456D');`}
+      </Script>
+
       <Header />
 
       <main className="grow">
--- app/(default1)/layout.tsx
+++ app/(default1)/layout.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Script from "next/script";
 
 import Header from "@/components/ui/header1";
 import Footer from "@/components/ui/footer1";
@@ -24,6 +25,18 @@ export default function DefaultLayout({
 
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-PV01NN456D"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-PV01NN456D');`}
+      </Script>
+
       <Header />
 
       <main className="grow">{children}</main>
